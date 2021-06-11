895e8fcf1a67563b5076cf76f6c0323ad435aeae
Daniel Kukieła <daniel@kukiela.pl>
2021-06-11 06:12:39 +0200

    Fixed missing backslash

--- scripts/gtav_inference.sh
+++ scripts/gtav_inference.sh
@@ -2,6 +2,6 @@
 
 python3 inference.py \
  --saved_model ./results/model3.pt \
- --data gtav:./data/gtav/data
+ --data gtav:./data/gtav/data \
  --inference_image_path ./data/gtav/2.png \
  --show_base_images True \
